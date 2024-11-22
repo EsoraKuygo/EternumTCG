6c05b67522e818038e759e391c1293ce146a58ff
Esora Kyugo <60149504+EsoraKuygo@users.noreply.github.com>
2024-11-22 22:19:54 +0100

    ajout connection base de donnée
    ajout des script de connection bdd avec singleton

---
+++ src/database/Connection.ts
@@ -0,0 +1,36 @@
+import { DataSource } from "typeorm";
+import dataSourceOptions from "./DataSourceOptions";
+
+export default class Connection {
+  private static instance: Connection | null = null;
+  private static initializing: Promise<void> | null = null;
+  private dataSource: DataSource = new DataSource(dataSourceOptions);
+
+  private constructor() {}
+
+  static async getInstance(): Promise<Connection> {
+    console.log(dataSourceOptions);
+    if (this.instance == null) {
+      if (!this.initializing) {
+        this.initializing = (async () => {
+          const instance = new Connection();
+          await instance.dataSource.initialize();
+          this.instance = instance;
+          this.initializing = null;
+        })();
+      }
+      await this.initializing;
+    }
+    if (!this.instance) {
+      throw new Error("Failed to initialize Connection");
+    }
+    return this.instance;
+  }
+
+  getDataSource() {
+    if (!this.dataSource.isInitialized) {
+      throw new Error("DataSource not initialized");
+    }
+    return this.dataSource;
+  }
+}
---
+++ src/database/DataSourceOptions.ts
@@ -0,0 +1,33 @@
+import { DataSourceOptions } from "typeorm";
+import dotenv from "dotenv";
+
+
+dotenv.config(); /* Charge les variables d'environnement. */
+const portDBContainer: number = parseInt(process.env.DB_PORT || "5432"); /* Port de la base de données. */
+const isDebug: boolean = process.env.DEBUG_TYPEORM === "true"; /* Détermine si le mode debug est activé. */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const isDevelopment: boolean =
+  process.env.NODE_ENV === "development"; /* Détermine si l'environnement est en mode développement. */
+
+/**
+ * Options de configuration pour la source de données interne.
+ */
+const dataSourceOptions: DataSourceOptions = {
+  type: "postgres" /* Type de la base de données. */,
+  host: process.env.DB_HOST /* Host de la base de données. */,
+  port: portDBContainer /* Port de la base de données. */,
+  username: process.env.DB_USER /* Nom d'utilisateur pour la connexion. */,
+  password: process.env.DB_PASS /* Mot de passe pour la connexion. */,
+  database: process.env.DB_NAME /* Nom de la base de données. */,
+  synchronize: true /* Synchronisation du schéma. */,
+  logging: isDebug /* Active les logs en mode debug. */,
+
+  /*  Entités à utiliser par TypeORM. */
+  entities: ["src/entities/**/*.ts"] /* Chemin vers les fichiers d'entité. */,
+
+  migrations: ["src/migrations/**/*.ts"] /* Chemin vers les fichiers de migration. */,
+  subscribers: ["src/subscribers/**/*.ts"] /* Chemin vers les fichiers de subscriber. */,
+};
+
+export default dataSourceOptions;
